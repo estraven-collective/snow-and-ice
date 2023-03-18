669163ec64cae3c3698da547c0d20472b8cbc1d1
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-03-18 12:22:22 +0100

    remove unused code

--- R/api-query.R
+++ R/api-query.R
@@ -1,12 +1,6 @@
 old_path <- Sys.getenv("PATH")
 Sys.setenv(PATH = paste(old_path, "/opt/homebrew/bin:/opt/homebrew/sbin", sep = ":"))
 
-# if(interactive()) {
-#   dotenv::load_dot_env(file = here::here('.env'))
-#   uid <- Sys.getenv('UID')
-#   pwd <- Sys.getenv('PWD')
-# }
-
 instrument <- 'VIIRS'
 data_id <- 'VNP10A1F'
 data_version <- '1'
@@ -21,8 +15,6 @@ curl_call <-
     '-L -O ',
     '--netrc-file .netrc ',
     '-J ',
-    # '--user "{uid}:{pwd}" ',
-    # '--digest ',
     '--dump-header response-header.txt ',
     '"https://n5eil02u.ecs.nsidc.org/egi/request?',
     'short_name={data_id}&',
@@ -31,7 +23,6 @@ curl_call <-
     'time={time}&',
     'page_size=100&',
     'agent=NO"'
-    # '"https://n5eil02u.ecs.nsidc.org/egi/request?short_name=SPL3SMP&version=007&format=GeoTIFF&time=2018-06-06,2018-06-07&bounding_box=-109,37,-102,41&bbox=-109,37,-102,41&Coverage=/Soil_Moisture_Retrieval_Data_AM/soil_moisture&projection=Geographic&page_size=100"'
   )
 
 system(
