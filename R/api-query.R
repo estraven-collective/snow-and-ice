2b890dafa1a4870375e56d389e6f6bbff72a8772
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-03-23 13:15:23 +0100

    script for api query to function

--- R/api-query.R
+++ R/api-query.R
@@ -6,130 +6,134 @@ Sys.setenv(PATH = paste(old_path, "/opt/homebrew/bin:/opt/homebrew/sbin", sep = 
 
 # parameters --------------------------------------------------------------
 
-
-instrument <- 'VIIRS'
-data_id <- 'VNP10A1F'
-data_version <- '1'
-format <- 'HDF-EOS5'
-time <- '2012-01-20T00:00:00,2023-03-21T12:00:00'
-W <- 6
-S <- 43
-E <- 14
-N <- 48
-WSEN <- glue::glue('{W},{S},{E},{N}')
-output_folder <- 'query_output'
-request_file <- 'request.xml'
-response_file <- 'response.xml'
-cookie_path <- here::here(output_folder, '.urs_cookies')
-auth_path <- here::here('.netrc')
-output_zip <- here::here(output_folder, 'output.zip')
-data_folder <- 'data'
-output_unzipped <- here::here(data_folder, 'query-full-timespan')
-
-# make a request to earthdata ---------------------------------------------
-
-curl_call <- 
-  glue::glue(
-    'curl ',
-    '-b {cookie_path} ',
-    '-c {cookie_path} ',
-    '-L ',
-    '-o "{output_folder}/{request_file}" ',
-    '--netrc-file {auth_path} ',
-    '-J ',
-    '--dump-header {output_folder}/response-header.txt ',
-    '"',
-    'https://n5eil02u.ecs.nsidc.org/egi/request?',
-    'request_mode=async&',
-    'short_name={data_id}&',
-    'version={data_version}&',
-    'format={format}&',
-    'time={time}&',
-    'page_size=100&',
-    'bounding_box={WSEN}&',
-    'bbox={WSEN}',
-    '"'
-  )
-
-system(
-  curl_call
-)
-
-
-# parse the request ID ----------------------------------------------------
-
-query_id <- system(
-  glue::glue(
-    "grep orderId {{output_folder}}/{{request_file}} | awk -F '<|>' '{print $3}'",
-    .open = '{{', .close = '}}'
-  ),
-  intern = T
-)
-
-
-# poll for status ---------------------------------------------------------
-
-id_poll <- 
-  glue::glue(
-    'curl ',
-    '-s ',
-    '-b {cookie_path} ',
-    '-c {cookie_path} ',
-    '-L ',
-    '--netrc-file {auth_path} ',
-    '-o "{output_folder}/{response_file}" ',
-    'https://n5eil02u.ecs.nsidc.org/egi/request/{query_id}'
-  )
-
-n_try <- 300
-
-for(i in 1:n_try) {
+fetch_viirs <- function(
+    instrument = 'VIIRS',
+    data_id = 'VNP10A1F',
+    data_version = '1',
+    format = 'HDF-EOS5',
+    time = '2012-01-20T00:00:00,2012-01-30T12:00:00',
+    W = 5,
+    S = 43,
+    E = 13,
+    N = 48,
+    output_folder = 'query_output',
+    request_file = 'request.xml',
+    response_file = 'response.xml',
+    cookie_path = here::here(output_folder, '.urs_cookies'),
+    auth_path = here::here('.netrc'),
+    output_zip = here::here(output_folder, 'output.zip'),
+    data_folder = 'data',
+    output_unzipped = here::here(data_folder, 'query-test'),
+    wait = 30
+) {
+  
+  WSEN <- glue::glue('{W},{S},{E},{N}')
+  
+  # make a request to earthdata ---------------------------------------------
+  
+  curl_call <- 
+    glue::glue(
+      'curl ',
+      '-b {cookie_path} ',
+      '-c {cookie_path} ',
+      '-L ',
+      '-o "{output_folder}/{request_file}" ',
+      '--netrc-file {auth_path} ',
+      '-J ',
+      '--dump-header {output_folder}/response-header.txt ',
+      '"',
+      'https://n5eil02u.ecs.nsidc.org/egi/request?',
+      'request_mode=async&',
+      'short_name={data_id}&',
+      'version={data_version}&',
+      'format={format}&',
+      'time={time}&',
+      'page_size=100&',
+      'bounding_box={WSEN}&',
+      'bbox={WSEN}',
+      '"'
+    )
+  
   system(
-    id_poll
+    curl_call
   )
   
-  response_status <- system(
+  
+  # parse the request ID ----------------------------------------------------
+  
+  query_id <- system(
     glue::glue(
-      "grep status {{output_folder}}/{{response_file}} | awk -F '<|>' '{print $3}'",
+      "grep orderId {{output_folder}}/{{request_file}} | awk -F '<|>' '{print $3}'",
       .open = '{{', .close = '}}'
     ),
     intern = T
   )
   
-  if(response_status == 'complete') {
-    cat(glue::glue('Done\n'))
-    break
-  }
-  else if(response_status == 'processing' | response_status == 'pending') {
-    cat(glue::glue('Tentative: {i}. Request status is: "{response_status}".\n'))
-    cat(glue::glue('Trying again in {wait} seconds...\n'))
-    Sys.sleep(wait)
-  } else {
-    stop('unknown response status: ', response_status)
+  
+  # poll for status ---------------------------------------------------------
+  
+  id_poll <- 
+    glue::glue(
+      'curl ',
+      '-s ',
+      '-b {cookie_path} ',
+      '-c {cookie_path} ',
+      '-L ',
+      '--netrc-file {auth_path} ',
+      '-o "{output_folder}/{response_file}" ',
+      'https://n5eil02u.ecs.nsidc.org/egi/request/{query_id}'
+    )
+  
+  n_try <- 300
+  
+  for(i in 1:n_try) {
+    system(
+      id_poll
+    )
+    
+    response_status <- system(
+      glue::glue(
+        "grep status {{output_folder}}/{{response_file}} | awk -F '<|>' '{print $3}'",
+        .open = '{{', .close = '}}'
+      ),
+      intern = T
+    )
+    
+    if(response_status == 'complete') {
+      cat('Done\n')
+      break
+    }
+    else if(response_status == 'processing' | response_status == 'pending') {
+      cat(glue::glue('Tentative: {i}. Request status is: "{response_status}".'), '\n')
+      cat(glue::glue('Trying again in {wait} seconds...'), '\n')
+      Sys.sleep(wait)
+    } else {
+      stop('unknown response status: ', response_status)
+    }
+    
   }
   
+  # download zipped output --------------------------------------------------
+  
+  download_out <- 
+    glue::glue(
+      'curl ',
+      '-s ',
+      '-b {cookie_path} ',
+      '-c {cookie_path} ',
+      '-L ',
+      '--netrc-file {auth_path} ',
+      '-o "{output_zip}" ',
+      'https://n5eil02u.ecs.nsidc.org/esir/{query_id}.zip'
+    )
+  
+  system(
+    download_out
+  )
+  
+  # unzip into data folder --------------------------------------------------
+  
+  return(output_zip)
 }
 
-# download zipped output --------------------------------------------------
-
-download_out <- 
-  glue::glue(
-    'curl ',
-    '-s ',
-    '-b {cookie_path} ',
-    '-c {cookie_path} ',
-    '-L ',
-    '--netrc-file {auth_path} ',
-    '-o "{output_zip}" ',
-    'https://n5eil02u.ecs.nsidc.org/esir/{query_id}.zip'
-  )
-
-system(
-  download_out
-)
-
-
-# unzip into data folder --------------------------------------------------
-
-unzip(zipfile = output_zip,
-      exdir = output_unzipped)
+zpath <- fetch_viirs()
